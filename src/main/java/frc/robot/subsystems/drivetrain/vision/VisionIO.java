435c7fc5d493e1c14ec64d7b12261a0d8b332a65
AlexNunemacher <apullinune@gmail.com>
2026-03-10 20:30:50 -0400

    vision

---
+++ src/main/java/frc/robot/subsystems/drivetrain/vision/VisionIO.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.drivetrain.vision;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.drivetrain.vision.LimelightHelpers.PoseEstimate;
+
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface VisionIO {
+  @AutoLog
+  public static class VisionIOInputs {
+    double targetX = 0;
+    double targetY = 0;
+    double targetA = 0;
+    boolean hasTarget = false;
+  }
+
+  public void updateInputs(VisionIOInputs inputs);
+
+  public PoseEstimate getPoseEstimateMT2();
+
+  public Pose2d getPose();
+
+  public void updateHeading(Supplier<Rotation2d> currentHeading);
+}
---
+++ src/main/java/frc/robot/subsystems/drivetrain/vision/VisionIOLimelight.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.drivetrain.vision;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.drivetrain.vision.LimelightHelpers.PoseEstimate;
+
+import java.util.function.Supplier;
+
+public class VisionIOLimelight implements VisionIO {
+
+  String cameraName = "";
+  Supplier<Rotation2d> currentHeading;
+
+  public VisionIOLimelight(String cameraName, Supplier<Rotation2d> currentHeading) {
+    this.cameraName = cameraName;
+    this.currentHeading = currentHeading;
+    LimelightHelpers.Flush();
+
+    LimelightHelpers.SetIMUMode(cameraName, 0);
+  }
+
+  @Override
+  public void updateInputs(VisionIOInputs inputs) {
+    LimelightHelpers.Flush();
+
+    inputs.targetX = LimelightHelpers.getTX(cameraName);
+    inputs.targetY = LimelightHelpers.getTY(cameraName);
+    inputs.targetA = LimelightHelpers.getTA(cameraName);
+    inputs.hasTarget = LimelightHelpers.getTV(cameraName);
+
+    updateHeading(currentHeading);
+  }
+
+  @Override
+  public PoseEstimate getPoseEstimateMT2() {
+    return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+  }
+
+  @Override
+  public Pose2d getPose() {
+    return getPoseEstimateMT2().pose;
+  }
+
+  @Override
+  public void updateHeading(Supplier<Rotation2d> currentHeading) {
+    LimelightHelpers.SetRobotOrientation(
+        cameraName, currentHeading.get().getDegrees(), 0, 0, 0, 0, 0);
+  }
+}
