6fde5250d81b48703eca180e76e9ff94715b3491
AlexNunemacher <apullinune@gmail.com>
2026-03-19 20:50:03 -0400

    modifies

---
+++ src/main/java/frc/robot/subsystems/hood/HoodSubsystem.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems.hood;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+public class HoodSubsystem extends SubsystemBase {
+
+  private final HoodIO io;
+  private final HoodIOInputsAutoLogged inputs = new HoodIOInputsAutoLogged();
+
+  public HoodSubsystem(HoodIO io) {
+    this.io = io;
+
+    setDefaultCommand(setHoodAngleForever(() -> HoodConstants.minHoodAngle));
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("RealOutputs/HoodSubsystem", inputs);
+  }
+
+  public Command setHoodAngle(Supplier<Rotation2d> desiredAngle) {
+    return runOnce(() -> io.setTargetAngle(desiredAngle.get()))
+        .andThen(Commands.run(() -> {}, this))
+        .until(this::isAtSetpoint);
+  }
+
+  public Command setHoodAngleForever(Supplier<Rotation2d> desiredAngle) {
+    return runOnce(() -> io.setTargetAngle(desiredAngle.get()));
+  }
+
+  @AutoLogOutput
+  public boolean isAtSetpoint() {
+    return io.isAtSetpoint();
+  }
+
+  public void setTargetAngle(Supplier<Rotation2d> desiredAngle) {
+    io.setTargetAngle(desiredAngle.get());
+  }
+
+  public Command setVoltage(double volts) {
+    return Commands.run(() -> io.setHoodVoltage(volts), this);
+  }
+
+  private Rotation2d getHoodAngleForDistance(double distanceMeters) {
+    double angleDeg = 20 + (distanceMeters * 5);
+
+    return Rotation2d.fromDegrees(angleDeg);
+  }
+
+  public Command aimHoodFromDistance(Supplier<Double> distanceMeters) {
+    return setHoodAngle(() -> getHoodAngleForDistance(distanceMeters.get()));
+  }
+
+  public Command aimHoodFromDistanceForever(Supplier<Double> distanceMeters) {
+    return setHoodAngleForever(() -> getHoodAngleForDistance(distanceMeters.get()));
+  }
+}
