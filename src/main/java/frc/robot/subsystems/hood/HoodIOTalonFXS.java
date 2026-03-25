0943de7181e212db980aa5dde50aae8dd08a2864
sjsonline <sjsonline@gmail.com>
2026-03-25 10:04:26 -0400

    Reverse and unjam

--- src/main/java/frc/robot/subsystems/hood/HoodIOTalonFXS.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOTalonFXS.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFXS;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class HoodIOTalonFXS implements HoodIO {
